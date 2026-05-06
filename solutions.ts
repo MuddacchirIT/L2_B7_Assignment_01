b0d30c318abff1b9251a5ac5796482b54d1fb2b0
unknown <protexbeer41@gmail.com>
2026-05-06 20:23:01 +0600

    first commit

---
+++ solutions.ts
@@ -0,0 +1,8 @@
+// Problem 1:
+
+function filterEvenNumbers(numbers: number[]): number[] {
+  return numbers.filter((num) => num % 2 === 0);
+}
+const allDigits = [1, 2, 3, 4, 5, 6];
+const evenDigits = filterEvenNumbers(allDigits);
+console.log(evenDigits);
